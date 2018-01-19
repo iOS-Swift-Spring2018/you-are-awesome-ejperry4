24c07f896f0f4c44232958209a7e06a3a3ef0cb2
Ej Perry <ejperry@ejs-macbook-pro.bc.edu>
2018-01-18 22:14:36 -0500

    Initial Commit

---
+++ You Are Awesome.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome.2
+//
+//  Created by Ej Perry on 1/18/18.
+//  Copyright © 2018 Perry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
