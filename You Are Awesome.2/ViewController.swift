2b09f5d81a8be27e44dd41411546e79590f44b78
Ej Perry <ejperry@ejs-macbook-pro.bc.edu>
2018-01-22 17:19:03 -0500

    First "You Are Awesome" Commit

--- You Are Awesome.2/ViewController.swift
+++ You Are Awesome.2/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var index = 0
+    @IBOutlet weak var awesomeMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showMessageButoon(_ sender: UIButton) {
+        
+//        let message1 = "You Are Da Bomb"
+//        let message2 = "You Are Great"
+//        let message3 = "You Are Amazing"
+//
+//        if awesomeMessage.text == message1 {
+//            awesomeMessage.text = message2
+//        } else if awesomeMessage.text == message2{
+//            awesomeMessage.text = message3
+//        } else {
+//            awesomeMessage.text = message1        }
+        let messages = ["You Are Da Bomb","You Are Great","You Are Amazing", "You belong in the genius bar"]
+        awesomeMessage.text = messages[index]
+        index = index + 1
+        if index == messages.count {
+            index = 0
+        }
     }
-
-
 }
 
