6adfad0cc5cd60565e1f135bb1c881c457c2bba9
Ej Perry <ejperry@ejs-macbook-pro.bc.edu>
2018-01-29 17:03:35 -0500

    Week 2

--- You Are Awesome.2/ViewController.swift
+++ You Are Awesome.2/ViewController.swift
@@ -7,16 +7,68 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var awesomeImages: UIImageView!
+    var awesomePlayer = AVAudioPlayer()
     var index = 0
+    var imageIndex = 0
+    let imageNum = 5
+    let soundNum = 5
+    var soundIndex = 0
+    
     @IBOutlet weak var awesomeMessage: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    func playSound(soundName: String) {
+        if let sound = NSDataAsset(name: soundName) {
+        //Check if sound.data is a sound file
+        do {
+        try awesomePlayer = AVAudioPlayer(data: sound.data)
+        awesomePlayer.play()
+    
+        } catch {
+        print("ERROR: Sound in sound\(soundName) could not be played as audio" )
+        }
+        }else {
+        // Error File Didn't Load
+        print("Error file sound\(soundName) didn't load" )
+        }
+    
+    
+    }
+    
+    func nonRepeatingRandomNumber (lastNumber: Int, maxValue: Int) -> Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        } while index == newIndex
+        return newIndex
+        
+    }
+    
     @IBAction func showMessageButoon(_ sender: UIButton) {
+        
+        let messages = ["You Are Da Bomb","You Are Great","You Are Amazing", "You belong in the genius bar"]
+        
+        //Show Messages
+        index = nonRepeatingRandomNumber(lastNumber: index, maxValue: messages.count)
+        awesomeMessage.text = messages[index]
+        awesomeImages.isHidden = false
+        
+        //Show Images
+        imageIndex = nonRepeatingRandomNumber(lastNumber: imageIndex, maxValue: imageNum)
+        awesomeImages.image = UIImage(named: "Image\(imageIndex)")
+        
+        //Play Sounds
+        soundIndex = nonRepeatingRandomNumber(lastNumber: soundIndex, maxValue: soundNum)
+        playSound(soundName: "sound\(soundIndex)")
+        
         
 //        let message1 = "You Are Da Bomb"
 //        let message2 = "You Are Great"
@@ -28,12 +80,14 @@ class ViewController: UIViewController {
 //            awesomeMessage.text = message3
 //        } else {
 //            awesomeMessage.text = message1        }
-        let messages = ["You Are Da Bomb","You Are Great","You Are Amazing", "You belong in the genius bar"]
+
+        /*
         awesomeMessage.text = messages[index]
         index = index + 1
         if index == messages.count {
             index = 0
+        */
         }
     }
-}
+
 
